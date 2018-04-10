147eb4c03756feb04614168565c76e40af043b12
Rob Prior <rrprior@gmail.com>
2018-04-10 18:56:39 +0100

    initial setup

---
+++ Pods/Target Support Files/Pods-WeJibe/Pods-WeJibe-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WeJibe : NSObject
+@end
+@implementation PodsDummy_Pods_WeJibe
+@end
---
+++ WeJibe/Helper files/Helper Files.swift
@@ -0,0 +1,69 @@
+//  MIT License
+
+//  Copyright (c) 2017 Haik Aslanyan
+
+import Foundation
+import UIKit
+
+//Global variables
+struct GlobalVariables {
+    static let blue = UIColor.rbg(r: 129, g: 144, b: 255)
+    static let purple = UIColor.rbg(r: 161, g: 114, b: 255)
+    static let red = UIColor.rbg(r: 255, g: 104, b: 117)
+    static let pink = UIColor.rbg(r: 255, g: 160, b: 144)
+}
+
+//Extensions
+extension UIColor{
+    class func rbg(r: CGFloat, g: CGFloat, b: CGFloat) -> UIColor {
+        let color = UIColor.init(red: r/255, green: g/255, blue: b/255, alpha: 1)
+        return color
+    }
+}
+
+class RoundedImageView: UIImageView {
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        let radius: CGFloat = self.bounds.size.width / 2.0
+        self.layer.cornerRadius = radius
+        self.clipsToBounds = true
+    }
+}
+
+class RoundedButton: UIButton {
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        let radius: CGFloat = self.bounds.size.height / 2.0
+        self.layer.cornerRadius = radius
+        self.clipsToBounds = true
+    }
+}
+
+//Enums
+enum ViewControllerType {
+    case welcome
+    case conversations
+}
+
+enum PhotoSource {
+    case library
+    case camera
+}
+
+enum ShowExtraView {
+    case contacts
+    case profile
+    case preview
+    case map
+}
+
+enum MessageType {
+    case photo
+    case text
+    case location
+}
+
+enum MessageOwner {
+    case sender
+    case receiver
+}
